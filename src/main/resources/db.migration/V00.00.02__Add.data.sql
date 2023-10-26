179fa66cea1bfbc1833efda65769941e0288a807
Davit Lomtatidze <davit.lomtatidze3@gmail.com>
2023-10-26 21:41:12 +0400

    add flyway

---
+++ src/main/resources/db.migration/V00.00.02__Add.data.sql
@@ -0,0 +1,11 @@
+insert into countries(id,iso_code_2,iso_code_3,name,name_geo)
+ values(6.DE,DEU,Germany,გერმანია);
+
+ insert into  users (id,email,first_name,last_name)
+values(6,'kapanadze6@gmail.com','maka','kapanadze');
+
+insert into  companies (id,parent_id,country_id,name,name_geo)
+values(6,555,'germany','xpraisi','ხპრაისი');
+
+insert into  products (id,ean,name,company_id,country_id)
+values(6,3303005004006,'vatuka',5,5);
